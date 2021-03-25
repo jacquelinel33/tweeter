f4addba69af8c6da8f811dd0ecba44ef81bf5d96
Jacqueline <jlee4332@gmail.com>
2021-03-25 19:01:07 -0400

    fix tweet post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,27 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
-$(document).ready(function() {
+ $(document).ready(function() {
   //create tweet elements and append to article. Return article.
   const createTweetElement = (object) => {
     const tweet = `<article class="tweet-article">
     <header>
-      <img src=${object.user.avatars}"> 
-      <span>${object.user.name}</span> 
-      <span id="disappear">${object.user.handle}</span>
+    <img src=${object.user.avatars}"> 
+    <span>${object.user.name}</span> 
+    <span id="disappear">${object.user.handle}</span>
     </header>
     <div class="tweet-body"> 
     ${object.content.text}
     </div>
     <footer class="tweet-foot">
-      <p>${new Date(object.created_at).toLocaleString()}</p>
-      <div class="icons">
-        <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/flag.png">
-        <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/retweet.png">
-        <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/heart.png">
-      </div>
+    <p>${new Date(object.created_at).toLocaleString()}</p>
+    <div class="icons">
+    <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/flag.png">
+    <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/retweet.png">
+    <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/heart.png">
+    </div>
     </footer>
-  </article>`
+    </article>`
     return tweet;
   };
-
+  
   //loop through tweets, calls createTweetElements and takes return value and appends it to tweets container
   const renderTweets = (tweetObjArr) => {
     for (let tweet of tweetObjArr) {
@@ -37,6 +30,17 @@ $(document).ready(function() {
     }
   };
 
+  //use ajax to get data from server
+  const loadTweets = (callback) => {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+    })
+      .then(res => callback(res));
+  };
+
+  loadTweets(renderTweets);
+  
   //post request that sends form data to server
   $('form').on('submit', function(event) {
     event.preventDefault();
@@ -49,7 +53,7 @@ $(document).ready(function() {
           method: "POST",
           data: tweet
         })
-        loadTweets(renderTweets);
+        location.reload();
         $('textarea').val('');
         $('.counter').val('140');
       }
@@ -65,23 +69,9 @@ $(document).ready(function() {
     }
     else {
       $(".error-empty").css({ 'display': 'block' });
-      // $('.counter').val('140');
-      // $('.counter').removeClass("red");
       $('form').on('click', function() {
         $(".error-empty").css({ 'display': 'none' });
       });
     }
   });
-
-  //use ajax to get data from server
-  const loadTweets = (callback) => {
-    $.ajax({
-      url: "http://localhost:8080/tweets",
-      method: "GET",
-    })
-      .then(res => callback(res));
-  };
-
-  loadTweets(renderTweets);
-
 });
