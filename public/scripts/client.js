81f302a7cb07677ca1340a382dde20481ea75f88
Jacqueline <jlee4332@gmail.com>
2021-03-30 21:10:09 -0400

    fix load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,51 +1,6 @@
- $(document).ready(function() {
-
-  const createTweetElement = (object) => {
-    const tweet = `
-    <article class="tweet-article">
-      <header>
-        <img src=${object.user.avatars}"> 
-        <span>${object.user.name}</span> 
-        <span id="disappear">${object.user.handle}</span>
-      </header>
-      <div class="tweet-body"> 
-        ${object.content.text}
-      </div>
-        <footer class="tweet-foot">
-        <p>${new Date(object.created_at).toLocaleString()}</p>
-      <div class="icons">
-       <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/flag.png">
-        <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/retweet.png">
-        <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/heart.png">
-      </div>
-      </footer>
-    </article>`
-
-    
-
-    return tweet;
-  };
-  
-  //loop through tweets, calls createTweetElements and takes return value and appends it to tweets container
-  const renderTweets = (tweetObjArr) => {
-    for (let tweet of tweetObjArr) {
-      const generatedTweets = createTweetElement(tweet);
-      $('.tweet-container').append(generatedTweets);
-    }
-  };
-
-  
-  const loadTweets = (callback) => {
-    $.ajax({
-      url: "http://localhost:8080/tweets",
-      method: "GET",
-    })
-      .then(res => callback(res));
-  };
-
+$(document).ready(function() {
   loadTweets(renderTweets);
-  
-  //post request to send form data to server
+
   $('form').on('submit', function(event) {
     event.preventDefault();
     const tweetInput = $('textarea').val();
@@ -56,26 +11,71 @@
           url: "/tweets/",
           method: "POST",
           data: tweet
-        })
-        location.reload();
+        }).then(() => loadTweets(renderTweets));
+
         $('textarea').val('');
         $('.counter').val('140');
-      }
-      else {
-        $('.error-length').css({ 'display': 'block' });
+      } else {
+        $('.error-length').css({
+          'display': 'block'
+        });
         $('textarea').val('');
         $('.counter').val('140');
         $('.counter').removeClass("red");
         $('form').on('click', function() {
-          $(".error-length").css({ 'display': 'none' });
+          $(".error-length").css({
+            'display': 'none'
+          });
         });
       }
-    }
-    else {
-      $(".error-empty").css({ 'display': 'block' });
+    } else {
+      $(".error-empty").css({
+        'display': 'block'
+      });
       $('form').on('click', function() {
-        $(".error-empty").css({ 'display': 'none' });
+        $(".error-empty").css({
+          'display': 'none'
+        });
       });
     }
   });
 });
+
+const createTweetElement = (object) => {
+  const tweet = `
+  <article class="tweet-article">
+    <header>
+      <img src=${object.user.avatars}"> 
+      <span>${object.user.name}</span> 
+      <span id="disappear">${object.user.handle}</span>
+    </header>
+    <div class="tweet-body"> 
+      ${object.content.text}
+    </div>
+      <footer class="tweet-foot">
+      <p>${new Date(object.created_at).toLocaleString()}</p>
+    <div class="icons">
+     <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/flag.png">
+      <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/retweet.png">
+      <img src="https://raw.githubusercontent.com/mpizzaca/tweeter/master/public/images/heart.png">
+    </div>
+    </footer>
+  </article>`
+
+  return tweet;
+};
+
+const renderTweets = (tweetObjArr) => {
+  for (let tweet of tweetObjArr) {
+    const generatedTweets = createTweetElement(tweet);
+    $('.tweet-container').append(generatedTweets);
+  }
+};
+
+const loadTweets = (callback) => {
+  $.ajax({
+    url: "http://localhost:8080/tweets",
+    method: "GET",
+  })
+    .then(res => callback(res));
+};
